4e115646f89f92a9ef690b2e32b681d4de7babb0
icantthinkofausernamesothiswilldo <161003294+icantthinkofausernamesothiswilldo@users.noreply.github.com>
2024-11-20 07:52:37 +0200

    fixed

--- src/main/java/com/github/x3rmination/common/enchantments/bow/EnchantmentPinDown.java
+++ src/main/java/com/github/x3rmination/common/enchantments/bow/EnchantmentPinDown.java
@@ -8,11 +8,10 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.MultiPartEntityPart;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
-
-import java.util.Objects;
 
 public class EnchantmentPinDown extends Enchantment {
     private static boolean handled = false;
@@ -42,16 +41,26 @@ public class EnchantmentPinDown extends Enchantment {
 
     @Override
     public void onEntityDamaged(EntityLivingBase user, Entity target, int level) {
-        if(Objects.requireNonNull(((EntityLivingBase) target).getLastDamageSource()).isProjectile() && !(user.isPotionActive(PotionInit.VENOM) || EnchantmentHelper.getEnchantmentLevel(EnchantmentInit.SOMBER, user.getItemStackFromSlot(EntityEquipmentSlot.LEGS)) > 0)) {
-            if (handled) {
-                handled = false;
-                return;
-            }
-            int potionDuration = (int) (((1.5 * Math.pow(level, 2)) - (2.5*level) + 4)*20);
-            PotionEffect pinDown = new PotionEffect(PotionInit.PIN_DOWN, potionDuration*20, 0, true, true);
+        if (target == null)
+            return;
 
-            ((EntityLivingBase) target).addPotionEffect(pinDown);
-            handled = true;
+        if (target instanceof EntityLivingBase)
+        {
+            if((((EntityLivingBase) target).getLastDamageSource()).isProjectile() && !(user.isPotionActive(PotionInit.VENOM) || EnchantmentHelper.getEnchantmentLevel(EnchantmentInit.SOMBER, user.getItemStackFromSlot(EntityEquipmentSlot.LEGS)) > 0)) {
+                if (handled) {
+                    handled = false;
+                    return;
+                }
+                int potionDuration = (int) (((1.5 * Math.pow(level, 2)) - (2.5*level) + 4)*20);
+                PotionEffect pinDown = new PotionEffect(PotionInit.PIN_DOWN, potionDuration*20, 0, true, true);
+
+                ((EntityLivingBase) target).addPotionEffect(pinDown);
+                handled = true;
+            }
+        } else if (target instanceof MultiPartEntityPart)
+        {
+            if (((MultiPartEntityPart) target).parent instanceof Entity)
+                onEntityDamaged(user, (Entity) ((MultiPartEntityPart) target).parent, level);
         }
     }
 }
